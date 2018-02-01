8ce858ac2c4d5c5d03cbd49d50754d070633d76a
lixiuhu <lixiuhu_lee@163.com>
2018-02-01 14:33:26 +0800

    change docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
-set -e
-
 if [ "$1" = 'production' ]; then
     yarn install
     if [ "$?" -gt 0 ]; then
-        pm2-docker start --env production /app/pm2.json
-    else
         echo "yarn install failed !!!!!!!!!!!"
+    else
+        pm2-docker start --env production /app/pm2.json
     fi
 fi
-
-exec "$@"
-
 
